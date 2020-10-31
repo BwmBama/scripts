85d6a686ed770a25bed8f9494162f5f170fdf962
BwmBama <66057563+BwmBama@users.noreply.github.com>
2020-10-31 14:01:11 +0800

    Update auto.sh

--- bash/auto.sh
+++ bash/auto.sh
@@ -4,8 +4,8 @@ source ~/.bashrc
 curl -o /usr/local/user/dockerauto-net.sh https://raw.githubusercontent.com/BwmBama/scripts/master/bash/dockerauto.sh --create-dirs
 if test -s /usr/local/user/dockerauto-net.sh; then
     rm -rf /usr/local/user/dockerauto.sh
-	mv /usr/local/user/dockerauto-net.sh /usr/local/user/dockerauto.sh
-	source /usr/local/user/dockerauto.sh > /tmp/auto.log
+    mv /usr/local/user/dockerauto-net.sh /usr/local/user/dockerauto.sh
+    source /usr/local/user/dockerauto.sh > /tmp/auto.log
 else
     source /usr/local/user/dockerauto.sh > /tmp/auto.log
 fi
